a35c9f6a4c593fcdec1faeb163e4f1f1a9a41ed0
jared-phillips <jp.james0927@gmail.com>
2019-09-28 23:56:59 +0000

    image blur 3

--- imageblur3.rb
+++ imageblur3.rb
@@ -1,24 +1,23 @@
 class Image
   attr_accessor :image
-
-  def initialize(image)
+  def initialize (image)
     self.image = image
   end
 
   def output_image
-    image.each do |row|
-      puts row.join("")
+    image.each do |rows|
+      puts rows.join("")
     end
   end
 
-  def blur(distance=1)
-    distance.times do
-      numbers
-    end
+def blur(distance)      
+  distance.times do
+    numbers
   end
-
-  def numbers
-    numbers = []
+end
+private 
+def numbers
+  numbers = []
     image.each_with_index do |row, i|
       row.each_with_index do |j, row_i|
         if j == 1
@@ -33,18 +32,23 @@ class Image
     image[number[0]][number[1] - 1] = 1 if number[1] - 1 >= 0
     image[number[0] + 1][number[1]] = 1 if number[0] + 1 <= image.length - 1
     image[number[0] - 1][number[1]] = 1 if number[0] - 1 >= 0
-    end 
-    end 
-  end
 
+        end
+      end
+end
 
-    
 
 image = Image.new([
-  [0, 1, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ])
-image.blur
+
+image.blur(3)
 image.output_image
